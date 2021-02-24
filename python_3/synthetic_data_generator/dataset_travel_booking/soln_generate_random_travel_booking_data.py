b08414fa04549aef594fe3a70042d7df3a59f11f
Ashish Dutt <ashish.dutt8@gmail.com>
2021-02-24 16:01:24 +0800

    create script
    - initial commit

---
+++ python_3/synthetic_data_generator/dataset_travel_booking/soln_generate_random_travel_booking_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 21 09:17:09 2021
+Last modified on Feb 24 21:00
+Objective: generate travel booking data
+Version: 0.0.1
+
+Note:
+1. Change the value for ROW_COUNT variable to generate n number of rows
+2. This script is the working version of script `expr_generate_random_travel_booking_data` 
+@author: Ashish
+"""
+
+
